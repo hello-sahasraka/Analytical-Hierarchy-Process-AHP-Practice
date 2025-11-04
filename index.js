ae8a9defc00ce84a77f0a98aaf51688375e8d1e5
hello-sahasraka <sahasrakasachin@gmail.com>
2025-11-04 13:53:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import ahpRouter from './src/routes/dataDrivenAHPRoutes.js';
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.use("/api/v1", ahpRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
